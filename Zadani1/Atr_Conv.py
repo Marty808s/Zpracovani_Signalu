361f55dd237db40bf2e4de6b91291979fa7b34f9
Marty808s <134133128+Marty808s@users.noreply.github.com>
2024-09-16 19:54:33 +0200

    update

---
+++ Zadani1/Atr_Conv.py
@@ -0,0 +1,47 @@
+import os
+import wfdb
+import numpy as np
+
+def calc_bpm(atr_data):
+    r_times = atr_data.sample / atr_data.fs
+    rr_distance = np.diff(r_times)
+    mean_rr = np.mean(rr_distance)
+    bpm = round(60 / mean_rr, 0)
+    return bpm
+
+def read_atr(data_path):
+    an = wfdb.rdann(data_path, 'atr')
+    if an:
+        return an
+    else:
+        raise ValueError("Nemám .atr data")
+
+    
+def run_batch():
+    path = 'Zadani1/TestData/test_data'
+
+    files = os.listdir(path)
+
+    files_to_read = [file for file in files if file.endswith('.atr')]
+    print(files_to_read)
+
+    result_dict = {}
+
+    if files_to_read:
+        for file in files_to_read:
+            file_name = file.split('.')[0]
+            print(f"file name: {file_name}")
+            path_to_file = os.path.join(path, file_name)
+            print(f"path: {path_to_file}")
+            an = read_atr(path_to_file)
+            bpm = calc_bpm(an)
+            print(bpm)
+            result_dict[file_name] = bpm
+    else:
+        print("Nemám .atr data")
+
+    print(result_dict)
+    return result_dict
+
+#res = run_batch()
+#print(res)
--- Zadani1/__1__.py
+++ Zadani1/__1__.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 import wfdb
 import matplotlib.pyplot as plt
+from Atr_Conv import run_batch
 
+get_batch = run_batch()
 
 def adaptive_threshold_median(signal, window_size):
     thresholds = []
@@ -55,6 +57,8 @@ def HearthRate(data_path):
     fs = fields['fs']
     print(f"Vzork. freq: {fs}")
 
+    #ECG_signal = ECG_signal[:100000]# omezení pro testování
+
     # Funkce pro adaptivní prahování založené na mediánu
 
     #  mediánového filtru k vyhlazení signálu
@@ -69,6 +73,7 @@ def HearthRate(data_path):
     R_peaks_indices = custom_find_peaks(ECG_smoothed, adaptive_thresholds, fs * 0.6)
 
     # Vykreslení signálu s detekovanými R-vrcholy a adaptivním prahem
+    """
     plt.figure(figsize=(12, 6))
     plt.plot(ECG_smoothed, label='Vyhlazený EKG signál')
     plt.plot(R_peaks_indices, ECG_smoothed[R_peaks_indices], 'ro', label='Detekované R-vrcholy')
@@ -79,16 +84,17 @@ def HearthRate(data_path):
     plt.legend()
     plt.grid(True)
     #plt.show()
-
+    """
     # Výpočet tepové frekvence
-    heart_rate = len(R_peaks_indices) / (len(ECG_smoothed) / fs) * 60
+    heart_rate = round(len(R_peaks_indices) / (len(ECG_smoothed) / fs) * 60, 0) #zaokrouhlení na celé BPM
     print("Detekováno R-vrcholů:", len(R_peaks_indices))
     print("Tepová frekvence:", heart_rate, "bpm")
 
     start_index = 0
     end_index = int(fs * 2)
 
-    # Vykreslení 2sekundového úseku signálu s detekovanými R-vrcholy a adaptivním prahem
+    # Vykreslení 2 sekundového úseku signálu s detekovanými R-vrcholy a adaptivním prahem
+    """
     plt.figure(figsize=(12, 6))
     plt.plot(ECG_smoothed[start_index:end_index], label='Vyhlazený EKG signál')
     plt.plot(R_peaks_indices[(R_peaks_indices >= start_index) & (R_peaks_indices < end_index)],
@@ -101,26 +107,52 @@ def HearthRate(data_path):
     plt.ylabel('Amplituda')
     plt.legend()
     plt.grid(True)
+    """
     return heart_rate
 
+# Funkce pro porovnání výsledné tepové frekvence s referenční hodnotou - rozdíl v bpm
+def compare(bpm1, bpm2):
+    dif_dict = {}
+    for id in bpm1.keys():
+        if id in bpm2:
+            diff = bpm1[id] - bpm2[id]
+            print(f"Rozdíl mezi vypočítanou a referenční hodnotou {id}: {diff} bpm")
+            dif_dict[id] = diff
+    return dif_dict
 
-lib_path = 'InputData'
+
+lib_path = 'Zadani1/TestData/test_data'
+csv_path = 'Zadani1'
+
 files = os.listdir(lib_path)
 drive_files = [file for file in files if file.endswith('.hea')]
 
-result_dict = {"Vzorek": [], "bpm": []}
+result_dict = {}
 
 for item in drive_files:
     file_name = os.path.splitext(item)[0]
     print(file_name)
     res = HearthRate(os.path.join(lib_path, file_name))
-    result_dict["Vzorek"].append(file_name)
-    result_dict["bpm"].append(res)
+    result_dict[file_name] = res
 
-print(len(result_dict["Vzorek"]))  # počet vzorků
-print(result_dict["Vzorek"])  # seznam názvů vzorků
-print(result_dict["bpm"])  # seznam výsledných bpm
 
-df_output = pd.DataFrame(data=result_dict)
-df_output.to_csv('out.csv', index=False)
+df_output = pd.DataFrame.from_dict(result_dict, orient='index', columns=['bpm'])
+df_output.reset_index(inplace=True)
+df_output.columns = ['id', 'bpm']
+df_output.to_csv(os.path.join(csv_path, 'out.csv'), index=False)
 print(df_output)
+
+
+comparsion = compare(result_dict, get_batch)
+print(comparsion)
+
+plt.figure(figsize=(12, 6))
+plt.plot(comparsion.keys(), comparsion.values(), marker='o', label='Diference mezi vypočítanou a referenční hodnotou BPM')
+plt.axhline(y=0, color='r', linestyle='--', label='y = 0')
+plt.grid(True)
+plt.legend()
+plt.title('Porovnání vypočítané a referenční tepové frekvence')
+plt.xlabel('ID záznamu')
+plt.ylabel('Rozdíl BPM')
+plt.show()
+
