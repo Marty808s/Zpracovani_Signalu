6872050aa7d5dc14df667fc72ebcd083b728190a
J-Viola <honza.viola@email.cz>
2024-09-20 17:53:38 +0200

    update

--- 2Zadani_1/gttstest.py
+++ 2Zadani_1/gttstest.py
@@ -1,4 +1,4 @@
-import gtts as ts
+import gtts
 
 word_list = ('time', 'prepare', 'solution', 'make', 'mistake', 'no', 'the', 'probable', 'long', 'lecture', 'method',
              'disaster', 'fail', 'work', 'advice', 'idea', 'succeed', 'easy', 'is', 'for', 'give')
