fae25c2fa8abe8e2ba8d263020808da55aaeea18
J-Viola <honza.viola@email.cz>
2024-09-21 12:59:05 +0200

    update seminar 2_1, added graphs

--- 2Zadani_1/Zvuk3.py
+++ 2Zadani_1/Zvuk3.py
@@ -9,16 +9,31 @@ import pandas as pd
 # Vzorkovací frekvence
 f = 22050
 
-# Slovník slov (poznámka: nepoužívejte 'dict' jako název proměnné)
+# Slovník slov
 word_list = ('time', 'prepare', 'solution', 'make', 'mistake', 'no', 'the', 'probable', 'long', 'lecture', 'method',
              'disaster', 'fail', 'work', 'advice', 'idea', 'succeed', 'easy', 'is', 'for', 'give')
 
+
 def load_signal(file_path):
+    """
+    Funkce pro načtení signálu
+    :param file_path: cesta k souboru se signálem
+    :return: pole, kde je po řádcích načtený signál
+    """
+
     with open(file_path, 'r') as file:
         signal_values = [float(line.strip()) for line in file.readlines()]
     return np.array(signal_values)
 
+
 def apply_hamming(signal, words):
+    """
+    Aplikace Hammingova okna na jednotlivá rozpoznaná slova rozpoznaná Hilbertovou tranformací. Vyhlazuje konce slov pro
+    lepší analýzu frekvenčního spektra.
+    :param signal: Signál v Hilbertově obálce
+    :param words: Indexy trvání jednotlivých identifikovaných slov
+    :return: Vyhlazený signál připravený na Fourierovu transformaci
+    """
     signal_with_hamming = np.zeros_like(signal)
     for start, end in words:
         # Aplikujeme Hammingovo okno na každé detekované slovo
@@ -29,21 +44,41 @@ def apply_hamming(signal, words):
         signal_with_hamming[start:end] = word_segment_hamming
     return signal_with_hamming
 
+
 def apply_hilbert(signal):
+    """
+    Vypočítá obálku amplitudy signálu pomocí Hilbertovy transformace.
+    :param signal: Vstupní signál
+    :return: Obálka amplitudy signálu
+    """
     analytic_signal = sig.hilbert(signal)
     amplitude_envelope = np.abs(analytic_signal)
     return amplitude_envelope
 
+
 def apply_fourier_transform(signal, f=f):
+    """
+    :param signal: Vyhlazený signál pomocí Hammingova okna
+    :param f: Vzorkovací frekvence signálu
+    :return: Frekvenční spektrum s jednotlivými obsaženými frekvencemi v signálu
+    """
     spectrum = np.fft.fft(signal)
     frequencies = np.fft.fftfreq(len(signal), d=1/f)
     return frequencies, spectrum
 
+
 def visualize_signal(signal, amplitude_envelope, words):
+    """
+    Vizualizuje vstupní signál spolu s vyhlazeným amplitudovým obalem
+    :param signal: Vstupní signál
+    :param amplitude_envelope: Vyhlazené (Hamming) amplitudové obaly (Hilbert)
+    :param words: Jednotlivá identifikovaná slova
+    """
     plt.figure(figsize=(12, 6))
     plt.plot(signal, label='Signál')
     plt.plot(amplitude_envelope, label='Amplitudový obal')
     plt.axhline(y=0.5, color='r', linestyle='-')
+    plt.axhline(y=threshold, color='k', linestyle='-', label='Treshold')
     for start, end in words:
         plt.axvspan(start, end, color='gray', alpha=0.5)
     plt.legend()
@@ -51,6 +86,12 @@ def visualize_signal(signal, amplitude_envelope, words):
 
 
 def identify_words(signal, threshold):
+    """
+    Identifikace jednotlivých slov pomocí přechodu přes definovaný treshold
+    :param signal: Vstupní signál
+    :param threshold: Námi zvolený treshold
+    :return: Indexy jednotlivých slov - začátek, konec
+    """
     words = []
     # Aplikace Hilbertovy transformace na signál pro získání analytického signálu
     amplitude_envelope = apply_hilbert(signal)
@@ -68,7 +109,14 @@ def identify_words(signal, threshold):
     words.append((word_start, crossings[-1]))
     return words
 
+
 def sort_values(signal, indexes):
+    """
+    Extrakce jednotlivých slov
+    :param signal: Vstupní signál
+    :param indexes: Identifikovaná slova
+    :return: Hodnoty signálu z indexů rozpoznaných slov
+    """
     vals = {}
     for num, i in enumerate(indexes):
         start_index, end_index = i
@@ -76,34 +124,20 @@ def sort_values(signal, indexes):
         vals[num] = sorted_val
     return vals
 
-def get_letters(word_list=word_list):
-    letters_in = []
-    for word in word_list:
-        for i in word:
-            if i not in letters_in:
-                letters_in.append(i)
-    return letters_in
-
-def create_spectral_bank(word_signals, f):
-    spectral_bank = {}
-    for word, signal in word_signals.items():
-        freq, spectrum = apply_fourier_transform(signal, f)
-        spectral_bank[word] = (freq, np.abs(spectrum))
-    return spectral_bank
 
 def correlate_spectra(spectrum1, spectrum2):
-    min_length = min(len(spectrum1), len(spectrum2))
-    spectrum1 = spectrum1[:min_length]
-    spectrum2 = spectrum2[:min_length]
+    """
+    Korelace spekter signálů
+    :param spectrum1: Vstupní signál ze zadání
+    :param spectrum2: Signál z nagenerované banky
+    :return: Maximální korelační koeficient
+    """
+    # Srovnání signálů na stejné délky
+    spectrum1 = spectrum1
+    spectrum2 = spectrum2
     correlation = np.correlate(spectrum1, spectrum2, mode='valid')
     return np.max(correlation)
 
-def visualize_spectral_comparison(freq, freq2, spectrum1, spectrum2, word_label):
-    plt.figure(figsize=(12, 6))
-    plt.plot(freq, np.abs(spectrum1), label='Spektrum vstupního slova')
-    plt.plot(freq2, np.abs(spectrum2), label=f'Spektrum slova "{word_label}" z banky')
-    plt.legend()
-    plt.show()
 
 def load_bank():
     lib_path = './Samples/gtts'
@@ -119,17 +153,24 @@ def load_bank():
         freq, spec = apply_fourier_transform(sig)
 
         fourier[item] = freq, spec
-        #plt.plot(freq, np.abs(spec))
-        #plt.show()
+    #   plt.plot(freq, np.abs(spec))
+    #   plt.show()
     return fourier
 
+
+def visualize_spectral_comparison(freq, freq2, spectrum1, spectrum2, word_label):
+    plt.figure(figsize=(12, 6))
+    plt.plot(freq, np.abs(spectrum1), label='Spektrum vstupního slova')
+    plt.plot(freq2, np.abs(spectrum2), label=f'Spektrum slova "{word_label}" z banky')
+    plt.legend()
+    plt.show()
 
 
 # Načtení signálu ze souboru
 signal_path = './InputData/Signal3.txt'
 signal = load_signal(signal_path)
 spectra_bank = load_bank()
-threshold = 13
+threshold = 25
 
 # Identifikace slov v signálu
 words = identify_words(signal, threshold)
@@ -142,26 +183,22 @@ print("Identifikovaná slova - indexy:", words, '\n', "Počet slov:", len(words)
 
 # Extrakce hodnot jednotlivých slov ze signálu
 sorted_words = sort_values(signal, words)
-
-# Získání seznamu unikátních písmen (pro informaci)
-letters = get_letters()
-print("Unikátní písmena ve slovníku:", letters, "Počet písmen:", len(letters))
+# Extrakce hodnot frekvenčního spektra a jednotlivých frekvencí obsažených v signálu
 frequence, fourier = apply_fourier_transform(signal, f=f)
 
-
+# Provedení korelace mezi signály ze vstupu a bankou
 cor = {}
 for i in range(len(words)):
     freq, spect = apply_fourier_transform(sorted_words[i])
-
     for j in spectra_bank.keys():
         specter = spectra_bank[j]
         spect2 = specter[1]
         freq2 = specter[0]
-        #print(spect)
+    #    print(spect)
         cor_coef = correlate_spectra(spect, spect2)
         cor[f"{i} | {j}"] = cor_coef
-        #visualize_spectral_comparison(freq, freq2,  spect, spect2, j)
-#print(cor)
+    #    visualize_spectral_comparison(freq, freq2,  spect, spect2, j)
+# print(cor)
 
 # Převod komplexího čísla na decimal..
 for key in cor:
@@ -172,7 +209,7 @@ for key, value in cor.items():
     sample_i, sample_j = key.split(' | ')
     data.append([sample_i, sample_j, value])
 
-# Dataframe pro tabulku
+# Dataframe pro tabulku a vizualizace
 df = pd.DataFrame(data, columns=['Sample_i', 'Sample_j', 'Correlation'])
 csv_path = "./Samples"
 df.to_csv(os.path.join(csv_path, 'cor_out.csv'), index=False)
